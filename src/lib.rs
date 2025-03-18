0c449c615ee3d58b821c4693be59364103e5dc1f
B.Billion <buhebillion@gmail.com>
2025-03-18 17:10:16 +0200

    case sensitivity

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,49 @@ pub fn run
 {
     let contents:String = fs::read_to_string(config.filePath)?;
 
-    println!("With text: \n {contents}");
-
+    for line in search(&config.query, &contents)
+    { println!("{line}"); }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+    #[test]
+    fn caseSensitive
+    ()
+    {
+        let query: &'static str = "duct";
+        let contents: &'static str =
+        "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn caseInsensitive
+    ()
+    {
+        let query: &'static str = "rUsT";
+        let contents: &'static str =
+        "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!
+        (
+            vec!["Rust:","Trust me."],
+            searchCaseInsensitive(query, contents)
+        );
+    }
 }
 
 impl Config
@@ -30,3 +70,34 @@ impl Config
 
 pub struct Config
 { pub query: String, pub filePath: String, }
+
+pub fn search
+<'a> (query: &str, contents:&'a str) -> Vec<&'a str>
+{
+    let mut results: Vec<&'a str> = Vec::new();
+
+    for line in contents.lines()
+    {
+        if line.contains(query)
+        { results.push(line); }
+    }
+
+    results
+}
+
+pub fn searchCaseInsensitive
+<'a> (query:&str, contents:&'a str,) -> Vec<&'a str>
+{
+    let query: String = query.to_lowercase();
+    let mut results: Vec<&'a str> = Vec::new();
+
+    for line in contents.lines()
+    {
+//ampersand to query coz contains() is defined as taking a string slice
+        if line.to_lowercase().contains(&query)
+        { results.push(line); }
+    }
+
+    results
+    //fn is not water proof according to Unicode standards!
+}
