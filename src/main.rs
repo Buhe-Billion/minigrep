7bde452f734f641d83af6dd0183a0e1002639551
B.Billion <buhebillion@gmail.com>
2025-03-18 09:01:08 +0200

    good comments

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,8 @@ fn main
     println!("Search for {query}");
     println!("In file {filePath}");
 
-    let contents:String  = fs::read_to_string(filePath)           // fs::read_to_string opens a file and returns std::io::Result<String>
+// fs::read_to_string opens a file and returns std::io::Result<String>
+    let contents:String  = fs::read_to_string(filePath)
     .expect("Should have been able to read the file");
 
     println!("With text: \n {contents}");
@@ -22,7 +23,7 @@ fn main
 fn parseConfig
 //&[String] reps &Vec<String>?!?! and maybe other collections with elements of String?
 (args: &[String]) -> (&str, &str)
-//(args: &str) was my initial set up, but we can't index into str with an int, UTF-8! 
+//(args: &str) was my initial set up, but we can't index into str with an int, UTF-8!
 {
     let query:&str = &args[1];
     let filePath:&str = &args[2];
