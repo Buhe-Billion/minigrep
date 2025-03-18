a011a2fa6db17a5523156d50a9c7dd4d006cd449
B.Billion <buhebillion@gmail.com>
2025-03-18 08:12:03 +0200

    ignore warnings

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+#![allow(warnings)]
+
 use std::env;
 
 fn main
 ()
 {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    dbg!(&args);
+
+    let query: &str = &args[1];
+    let filePath = &args[2];
+
+    println!("Search for {query}");
+    println!("In file {filePath}");
 }
