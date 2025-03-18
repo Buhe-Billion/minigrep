1398d87bbe6c19f92c1da1edaf715856a669b0d1
B.Billion <buhebillion@gmail.com>
2025-03-18 09:15:18 +0200

    {0} in println macro, still needs discovery

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,28 @@ fn main
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
-    let (query,filePath): (&str, &str) = parseConfig(&args);
+//    let (query,filePath): (&str, &str) = parseConfig(&args);
 
-    println!("Search for {query}");
-    println!("In file {filePath}");
+    let config: Config = parseConfig(&args);
 
+    println!("Search for {0}",config.query);
+    println!("In file {0}",config.filePath); //The 0 here is an index!!??!! wow!
+
+/*
 // fs::read_to_string opens a file and returns std::io::Result<String>
     let contents:String  = fs::read_to_string(filePath)
     .expect("Should have been able to read the file");
+*/
+
+    let contents:String = fs::read_to_string(config.filePath)
+    .expect("Shoulda been able to read the file");
 
     println!("With text: \n {contents}");
 }
 
+/*
 fn parseConfig
-//&[String] reps &Vec<String>?!?! and maybe other collections with elements of String?
+//&[String] reps a Vec<String> element borrow?!?! and maybe other collections with elements of String?
 (args: &[String]) -> (&str, &str)
 //(args: &str) was my initial set up, but we can't index into str with an int, UTF-8!
 {
@@ -29,4 +37,20 @@ fn parseConfig
     let filePath:&str = &args[2];
 
     (query,filePath)
+}
+*/
+
+fn parseConfig
+(args: &[String]) -> Config
+{
+    let query = args[1].clone();
+    let filePath = args[2].clone();
+
+    Config {query, filePath }
+}
+
+struct Config
+{
+    query: String,
+    filePath: String,
 }
